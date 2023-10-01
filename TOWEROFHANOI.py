d36bc13ec3076fe51f241b91756b3821ea5b399b
adupatil161 <138880914+adupatil161@users.noreply.github.com>
2023-10-01 10:54:39 +0530

    Add files via upload

---
+++ TOWEROFHANOI.py
@@ -0,0 +1,25 @@
+def move(a,b):
+    print("move",a,"to",b)
+
+def TowerOfHanoi(n,A,B,C):
+    if n==1:
+        move(A,C)
+
+    else:
+        TowerOfHanoi(n-1,A,C,B)
+        move(A,C)
+        TowerOfHanoi(n-1,B,A,C)
+n=(int(input("enter the number of disks:")))
+TowerOfHanoi(n,"A","B","C")
+
+
+
+#OUTPUT
+enter the number of disks:3
+move A to C
+move A to B
+move C to B
+move A to C
+move B to A
+move B to C
+move A to C
